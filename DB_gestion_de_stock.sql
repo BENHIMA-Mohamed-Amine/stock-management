6da027b5e5363d07f89e79f15bdf3f7eeb00db20
Mohamed-amine <benhima.mohamed.amine@gmail.com>
2022-12-05 20:11:51 +0100

    ajouter la table marque

--- DB_gestion_de_stock.sql
+++ DB_gestion_de_stock.sql
@@ -1,88 +1,301 @@
-DROP DATABASE IF EXISTS gestion_des_stocks;
-CREATE DATABASE gestion_des_stocks;
-USE gestion_des_stocks;
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Dec 05, 2022 at 08:09 PM
+-- Server version: 10.4.25-MariaDB
+-- PHP Version: 8.1.10
 
-DROP TABLE IF EXISTS Categorie ;
-CREATE TABLE Categorie (
-	id_cat INT PRIMARY KEY AUTO_INCREMENT,
-	lib_cat VARCHAR(100)
-);
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
 
-DROP TABLE IF EXISTS Produit;
-CREATE TABLE Produit (
-	num_pr VARCHAR(100) PRIMARY KEY,
-	id_cat INT,
-	lib_pr VARCHAR(10000),
-	prix_uni FLOAT,
-	prix_vente FLOAT,
-	prix_achat FLOAT,
-	qte_stock INT, 
-	CONSTRAINT fk_categorie FOREIGN KEY (id_cat) REFERENCES Categorie(id_cat) ON UPDATE CASCADE ON DELETE CASCADE
-);
 
-DROP TABLE IF EXISTS Client;
-CREATE TABLE Client (
-	id INT PRIMARY KEY AUTO_INCREMENT,
-	nom VARCHAR(100),
-	prenom VARCHAR(100),
-	adr VARCHAR(1000),
-	tele VARCHAR(20),
-	email VARCHAR(1000)
-);
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
 
-DROP TABLE IF EXISTS Fournisseur;
-CREATE TABLE Fournisseur (
-	id INT PRIMARY KEY AUTO_INCREMENT,
-	nom VARCHAR(100),
-	prenom VARCHAR(100),
-	adr VARCHAR(1000),
-	tele VARCHAR(20),
-	email VARCHAR(1000)
-);
+--
+-- Database: `gestion_des_stocks`
+--
 
-DROP TABLE IF EXISTS Admin;
-CREATE TABLE Admin (
-    id INT PRIMARY KEY AUTO_INCREMENT,
-	nom VARCHAR(100),
-	prenom VARCHAR(100),
-	adr VARCHAR(1000),
-	tele VARCHAR(20),
-	email VARCHAR(1000),
-	mdp VARCHAR(1000)
-);
+-- --------------------------------------------------------
 
-DROP TABLE IF EXISTS Commande;
-CREATE TABLE Commande (
-	num_com VARCHAR(50) PRIMARY KEY,
-	date_com DATE,
-	id_cli INT,
-	CONSTRAINT fk_com_cli FOREIGN KEY (id_cli) REFERENCES Client(id) ON UPDATE CASCADE ON DELETE CASCADE
-);
+--
+-- Table structure for table `admin`
+--
 
-DROP TABLE IF EXISTS Contient_pr ;
-CREATE TABLE Contient_pr (
-	num_pr VARCHAR(100),
-	num_com VARCHAR(50),
-	qte_pr INT,
-	PRIMARY KEY (num_pr,num_com),
-	CONSTRAINT fk_com_pr FOREIGN KEY (num_com) REFERENCES Commande(num_com) ON UPDATE CASCADE ON DELETE CASCADE,
-	CONSTRAINT fk_pr_com FOREIGN KEY (num_pr) REFERENCES Produit(num_pr) ON UPDATE CASCADE ON DELETE CASCADE
-);
+CREATE TABLE `admin` (
+  `id` int(11) NOT NULL,
+  `nom` varchar(100) DEFAULT NULL,
+  `prenom` varchar(100) DEFAULT NULL,
+  `adr` varchar(1000) DEFAULT NULL,
+  `tele` varchar(20) DEFAULT NULL,
+  `email` varchar(1000) DEFAULT NULL,
+  `mdp` varchar(1000) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
-DROP TABLE IF EXISTS Approvisionnement ;
-CREATE TABLE Approvisionnement (
-	num_app VARCHAR(50) PRIMARY KEY,
-	date_app DATE,
-	id_four INT,
-	CONSTRAINT fk_four_app FOREIGN KEY (id_four) REFERENCES Fournisseur(id) ON UPDATE CASCADE ON DELETE CASCADE
-);
+-- --------------------------------------------------------
 
-DROP TABLE IF EXISTS est_compose ;
-CREATE TABLE est_compose (
-	num_app VARCHAR(50),
-	num_pr VARCHAR(100),
-	qte_achete INT,
-	PRIMARY KEY (num_app,num_pr),
-	CONSTRAINT fk_app_pr FOREIGN KEY (num_app) REFERENCES Approvisionnement(num_app) ON UPDATE CASCADE ON DELETE CASCADE,
-	CONSTRAINT fk_pr_app FOREIGN KEY (num_pr) REFERENCES Produit(num_pr) ON UPDATE CASCADE ON DELETE CASCADE
-);
+--
+-- Table structure for table `approvisionnement`
+--
+
+CREATE TABLE `approvisionnement` (
+  `num_app` varchar(50) NOT NULL,
+  `date_app` date DEFAULT NULL,
+  `id_four` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `categorie`
+--
+
+CREATE TABLE `categorie` (
+  `id_cat` int(11) NOT NULL,
+  `lib_cat` varchar(100) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `client`
+--
+
+CREATE TABLE `client` (
+  `id` int(11) NOT NULL,
+  `nom` varchar(100) DEFAULT NULL,
+  `prenom` varchar(100) DEFAULT NULL,
+  `adr` varchar(1000) DEFAULT NULL,
+  `tele` varchar(20) DEFAULT NULL,
+  `email` varchar(1000) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `commande`
+--
+
+CREATE TABLE `commande` (
+  `num_com` varchar(50) NOT NULL,
+  `date_com` date DEFAULT NULL,
+  `id_cli` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `contient_pr`
+--
+
+CREATE TABLE `contient_pr` (
+  `num_pr` varchar(100) NOT NULL,
+  `num_com` varchar(50) NOT NULL,
+  `qte_pr` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `est_compose`
+--
+
+CREATE TABLE `est_compose` (
+  `num_app` varchar(50) NOT NULL,
+  `num_pr` varchar(100) NOT NULL,
+  `qte_achete` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `fournisseur`
+--
+
+CREATE TABLE `fournisseur` (
+  `id` int(11) NOT NULL,
+  `nom` varchar(100) DEFAULT NULL,
+  `prenom` varchar(100) DEFAULT NULL,
+  `adr` varchar(1000) DEFAULT NULL,
+  `tele` varchar(20) DEFAULT NULL,
+  `email` varchar(1000) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `marque`
+--
+
+CREATE TABLE `marque` (
+  `id_marque` int(11) NOT NULL,
+  `nom_marque` varchar(1000) NOT NULL,
+  `description_marque` varchar(5000) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `produit`
+--
+
+CREATE TABLE `produit` (
+  `num_pr` varchar(100) NOT NULL,
+  `id_cat` int(11) DEFAULT NULL,
+  `id_marque` int(11) NOT NULL,
+  `lib_pr` varchar(10000) DEFAULT NULL,
+  `prix_uni` float DEFAULT NULL,
+  `prix_vente` float DEFAULT NULL,
+  `prix_achat` float DEFAULT NULL,
+  `qte_stock` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `admin`
+--
+ALTER TABLE `admin`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `approvisionnement`
+--
+ALTER TABLE `approvisionnement`
+  ADD PRIMARY KEY (`num_app`),
+  ADD KEY `fk_four_app` (`id_four`);
+
+--
+-- Indexes for table `categorie`
+--
+ALTER TABLE `categorie`
+  ADD PRIMARY KEY (`id_cat`);
+
+--
+-- Indexes for table `client`
+--
+ALTER TABLE `client`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `commande`
+--
+ALTER TABLE `commande`
+  ADD PRIMARY KEY (`num_com`),
+  ADD KEY `fk_com_cli` (`id_cli`);
+
+--
+-- Indexes for table `contient_pr`
+--
+ALTER TABLE `contient_pr`
+  ADD PRIMARY KEY (`num_pr`,`num_com`),
+  ADD KEY `fk_com_pr` (`num_com`);
+
+--
+-- Indexes for table `est_compose`
+--
+ALTER TABLE `est_compose`
+  ADD PRIMARY KEY (`num_app`,`num_pr`),
+  ADD KEY `fk_pr_app` (`num_pr`);
+
+--
+-- Indexes for table `fournisseur`
+--
+ALTER TABLE `fournisseur`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `marque`
+--
+ALTER TABLE `marque`
+  ADD PRIMARY KEY (`id_marque`);
+
+--
+-- Indexes for table `produit`
+--
+ALTER TABLE `produit`
+  ADD PRIMARY KEY (`num_pr`),
+  ADD KEY `fk_categorie` (`id_cat`),
+  ADD KEY `fk_marque` (`id_marque`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `admin`
+--
+ALTER TABLE `admin`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `categorie`
+--
+ALTER TABLE `categorie`
+  MODIFY `id_cat` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `client`
+--
+ALTER TABLE `client`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `fournisseur`
+--
+ALTER TABLE `fournisseur`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `marque`
+--
+ALTER TABLE `marque`
+  MODIFY `id_marque` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `approvisionnement`
+--
+ALTER TABLE `approvisionnement`
+  ADD CONSTRAINT `fk_four_app` FOREIGN KEY (`id_four`) REFERENCES `fournisseur` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Constraints for table `commande`
+--
+ALTER TABLE `commande`
+  ADD CONSTRAINT `fk_com_cli` FOREIGN KEY (`id_cli`) REFERENCES `client` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Constraints for table `contient_pr`
+--
+ALTER TABLE `contient_pr`
+  ADD CONSTRAINT `fk_com_pr` FOREIGN KEY (`num_com`) REFERENCES `commande` (`num_com`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `fk_pr_com` FOREIGN KEY (`num_pr`) REFERENCES `produit` (`num_pr`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Constraints for table `est_compose`
+--
+ALTER TABLE `est_compose`
+  ADD CONSTRAINT `fk_app_pr` FOREIGN KEY (`num_app`) REFERENCES `approvisionnement` (`num_app`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `fk_pr_app` FOREIGN KEY (`num_pr`) REFERENCES `produit` (`num_pr`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Constraints for table `produit`
+--
+ALTER TABLE `produit`
+  ADD CONSTRAINT `fk_marque` FOREIGN KEY (`id_marque`) REFERENCES `marque` (`id_marque`) ON DELETE CASCADE ON UPDATE CASCADE;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
