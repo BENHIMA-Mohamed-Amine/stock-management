69c784cfd42b3474ef276bcaaba0e3653e936ed9
Haitam Belcaida <haitambelcaida@gmail.com>
2022-12-25 22:43:20 +0100

    version finale

--- gestion_des_stocks.sql
+++ gestion_des_stocks.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Dec 25, 2022 at 04:48 PM
+-- Generation Time: Dec 25, 2022 at 10:42 PM
 -- Server version: 10.4.24-MariaDB
 -- PHP Version: 8.1.6
 
